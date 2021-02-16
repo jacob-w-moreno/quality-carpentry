2dc4dadc8209b151dce790984ab2b3c858d5e025
Jacob Moreno <squidslippers@Jacobs-MacBook-Pro.local>
2021-02-16 15:09:12 -0700

    Header is nearly fully responsive (the responsive dropdown doesn't ease in or out yet).

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
+import Aux from './Aux';
 import Logo from '../Styles/Assets/Logo Smallest.png';
 import Dropdown from '../Styles/Assets/Icon Dropdown.png';
 import Hamburger from '../Styles/Assets/Icon Hamburger.png';
@@ -11,6 +12,7 @@ const Header = props => {
     "Roofing", "Flooring", "Remodels", "Basements",
     "Tree Removal", "Other"
   ];
+
   const servicesDropdown = <div className='header_dropdown'>
     {services.map((element, index) => {
       return <div className='header_dropdown-item' key={index+element}>
@@ -18,7 +20,18 @@ const Header = props => {
       </div>
     })}
   </div>
-  return <div className='header'>
+
+  const hamburgerDropdown = <div className='header_dropdown-2'>
+    {services.map((element, index) => {
+      return <div className='header_dropdown-item-2' key={index+element}>
+        {element}
+      </div>
+    })}
+    <button className='header_button-2 button_cta'>GET A QUOTE</button>
+  </div>
+
+  return <Aux>
+  <div className='header'>
     <Link to='/'>
       <div className='header_logo'>
         <img className='header_logo-img' src={Logo} alt="Quality Carpentry Logo"/>
@@ -26,29 +39,38 @@ const Header = props => {
       </div>
     </Link>
     <div className='header_nav-container'>
-    <nav>
-      <ul>
-        <li className='header_link'>
-          <Link to='/'> HOME </Link>
-        </li>
-        <li className='header_link header_dropdown-container'>
-            SERVICES 
-            <img 
-              className='header_dropdown-caret'
-              src={Dropdown}
-              alt={'Dropdown Caret'}
-            />
-        { servicesDropdown }
-        </li>
-        <li className='header_link'>
-          <Link to='/about'> ABOUT </Link>
-        </li>
-      </ul>
-    </nav>
-    <img className='header_hamburger' src={Hamburger} alt='hamburger menu'/>
-    <button className='header_button button_cta'>GET A QUOTE</button>
+      <nav>
+        <ul>
+          <li className='header_link'>
+            <Link to='/'> HOME </Link>
+          </li>
+          <li className='header_link header_dropdown-container'>
+              SERVICES 
+              <img 
+                className='header_dropdown-caret'
+                src={Dropdown}
+                alt={'Dropdown Caret'}
+                />
+          { servicesDropdown }
+          </li>
+          <li className='header_link'>
+            <Link to='/about'> ABOUT </Link>
+          </li>
+        </ul>
+      </nav>
+      <img className='header_hamburger' 
+        src={Hamburger} 
+        alt='hamburger menu'
+        onClick={()=>dropdown?setDropdown(false):setDropdown(true)}
+      />
+      <button className='header_button button_cta'>GET A QUOTE</button>
     </div>
   </div>
+  {dropdown
+    ? hamburgerDropdown
+    : null
+  }
+  </Aux>
 }
 
 export default Header;
