6d7765ac4211038c3e971805da69315a3f23a187
Jacob Moreno <jacob.w.moreno@gmail.com>
2021-02-18 11:05:24 -0700

    Scroll to top added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './Styles/_style.scss';
 import routes from './routes';
 
 import Header from './Components/Header';
+import ScrollToTop from './Components/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop/>
       <Header/>
       {routes}
     </div>
