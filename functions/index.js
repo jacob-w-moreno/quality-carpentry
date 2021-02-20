24ba02eca263784a8e9c93d2d6fc06090c10c95f
Jacob Moreno <jacob.w.moreno@gmail.com>
2021-02-20 12:52:00 -0700

    Getting all emails and creating new emails works.

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,50 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
-  console.log('console.log helloWorld');
-  res.json({result: 'WIN: helloWorld'});
+admin.initializeApp();
+const db = admin.firestore();
+// const app = require('express')();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello world');
 });
 
-exports.testFunction = functions.https.onCall((data, context) => {
-  console.log('console.log testFunction');
-  console.log('data:', data);
-  return {
-    status: 'complete',
-    finished: true,
-    error: false
+exports.getEmails = functions.https.onRequest((req, res) => {
+  db
+    .collection('emails')
+    .get()
+    .then(data => {
+      let emails = [];
+      data.forEach(doc => {
+        emails.push(
+          doc.data()
+          // email: doc.data().email,
+          // message: doc.data().message,
+          // name: doc.data().name,
+          // phone: doc.data().phone
+        );
+      })
+      return res.json(emails);
+    })
+    .catch(err => console.error(err));
+});
+
+exports.createEmail = functions.https.onRequest((req, res) => {
+  const newEmail = {
+    email: req.body.email,
+    message: req.body.message,
+    name: req.body.name,
+    phone: req.body.phone
   }
-})
+
+  db
+    .collection('emails')
+    .add(newEmail)
+    .then(doc => {
+      res.json({ message: `Email ${doc.id} sent successfully!`})
+    })
+    .catch(err => {
+      res.status(500).json({ error: `Something went wrong... Please try again.` });
+      console.error(err);
+    })
+});
