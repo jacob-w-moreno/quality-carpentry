40b18d9456de791d95622e3172cc649ed071ae52
Jacob Moreno <jacob.w.moreno@gmail.com>
2021-03-11 15:14:53 -0700

    Added the 'Other' bubble back

--- src/Components/Services/ServicesList/Services.js
+++ src/Components/Services/ServicesList/Services.js
@@ -6,7 +6,7 @@ import Flooring from '../../../Styles/Assets/Icons/Flooring.png';
 import Remodels from '../../../Styles/Assets/Icons/Remodels.png';
 import Roofing from '../../../Styles/Assets/Icons/Roofing.png';
 import TreeRemoval from '../../../Styles/Assets/Icons/TreeRemoval.png';
-// import Other from '../../../Styles/Assets/Icons/Other.png';
+import Other from '../../../Styles/Assets/Icons/Other.png';
 import JunkRemoval from '../../../Styles/Assets/Icons/JunkRemoval.png';
 
 const Services = props => {
@@ -17,8 +17,8 @@ const Services = props => {
     {service: 'Remodels', image: Remodels},
     {service: 'Basements', image: Basements},
     {service: 'Tree Removal', image: TreeRemoval},
-    {service: 'Junk Removal', image: JunkRemoval}
-    // {service: 'Other', image: Other},
+    {service: 'Junk Removal', image: JunkRemoval},
+    {service: 'Other', image: Other}
   ];
 
   return <div className='landing_services' style={{background: props.style}}>
